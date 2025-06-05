5094fb9311278161a7db910360fbbdc22b399400
tim <tradtke22@gmail.com>
2025-06-05 16:00:21 +0000

    adding some new keymaps and fixing line numbers

--- lua/config/editor-options.lua
+++ lua/config/editor-options.lua
@@ -9,6 +9,25 @@ vim.g.mapleader = " "
 
 -- line numbers
 vim.opt.relativenumber = true
+-- Enable absolute line numbers -- changes 0 on the current line to the actual line number
+vim.opt.number = true
+
+-- Enable relative line numbers by default
+
+-- Autocmd to switch off relative numbers when entering Insert mode
+vim.api.nvim_create_autocmd('InsertEnter', {
+  group = vim.api.nvim_create_augroup('NumberToggleInsertEnter', { clear = true }),
+  callback = function()
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  group = vim.api.nvim_create_augroup('NumberToggleInsertLeave', { clear = true }),
+  callback = function()
+    vim.opt.relativenumber = true
+  end,
+})
 
 -- autocommand for diagnostics
 -- vim.api.nvim_create_autocmd("ModeChanged", {
@@ -19,3 +38,5 @@ vim.opt.relativenumber = true
 -- })
 
 vim.keymap.set("n", "<C-e>", "<cmd>Oil<CR>")
+-- ensures that ctrl c replicates the same events when using it to get out of insert, usually use ctrl c on a laptop
+vim.keymap.set("i", "<C-c>", "<Esc>")
