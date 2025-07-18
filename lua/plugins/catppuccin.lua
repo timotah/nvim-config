740d450e7565b9014d8e8c7f190f5f0d4abaa472
Tim Radtke <tradtke22@gmail.com>
2025-07-18 14:35:33 -0500

    fixing blink.cmp, its working, need styles

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,7 +4,7 @@ return {
   name = "catppuccin",
   priority = 1000,
   opts = {
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
     terminal_colors = true,
     styles = {
       comments = { "italic" },
@@ -37,7 +37,7 @@ return {
     },
   },
   config = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
+    vim.cmd.colorscheme "catppuccin-macchiato"
   end,
 }
 
