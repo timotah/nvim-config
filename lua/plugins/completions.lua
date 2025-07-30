ea69f3a6b0ca308bc9045d9c5bb93a5f843b422b
tim <tradtke22@gmail.com>
2025-07-30 14:29:11 +0000

    adding some blink settings

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -4,10 +4,24 @@ return {
 	-- `main` is untested, please open a PR if you've confirmed it works as expected
 	dependencies = { { "L3MON4D3/LuaSnip", version = "v2.*" }, { "rafamadriz/friendly-snippets" } },
 	opts = {
+    completion = {
+      documentation = {
+        auto_show = true,
+      }
+    },
 		snippets = { preset = "luasnip" },
 		-- ensure you have the `snippets` source (enabled by default)
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
+      providers = {
+        lsp = {
+          name = "LSP",
+          enabled = true,
+          async = true,
+          timeout_ms = 500,
+          max_items = 5,
+        },
+      },
 		},
 	},
 }
