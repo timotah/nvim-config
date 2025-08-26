8ff38652272b614517e9b7709dc791bcf0b3b0b0
tim <tradtke22@gmail.com>
2025-08-26 17:19:14 +0000

    treesitter context changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,20 @@
 return {
  'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  config = function()
-    local config = require('nvim-treesitter.configs')
-    config.setup({
-      auto_install = true,
-      highlight = { enable = true, additional_vim_regex_highlighting = false },
-      indent = { enable = true }
-    })
-  end
+  opts = {
+    auto_install = true,
+    highlight = { enable = true, additional_vim_regex_highlighting = false },
+    indent = { enable = true }
+  },
+  -- dependencies = {
+  --   'nvim-treesitter/nvim-treesitter-context',
+  -- },
+  -- config = function()
+  --   local config = require('nvim-treesitter.configs')
+  --   config.setup({
+  --     auto_install = true,
+  --     highlight = { enable = true, additional_vim_regex_highlighting = false },
+  --     indent = { enable = true }
+  --   })
+  -- end
 }
